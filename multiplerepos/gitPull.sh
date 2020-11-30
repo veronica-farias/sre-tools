ee0d4dbf42440ac3e03a2821d8b281004d8beeb2
veronica-farias <a30005905@agl.com.au>
2020-12-01 10:16:03 +1100

    Added comments about how to run it

--- multiplerepos/gitPull.sh
+++ multiplerepos/gitPull.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
-### script to clone multiple git repositories specified in an input file
+# script to pull multiple git repositories and update submodules specified in an input file
+# run: ./gitClone.sh $inputFile $mainDirMultiRepoPath
+# i.e. unix-like: sh gitClone.sh nembidRepo.list ~/repo/nem
+# i.e. poweshell: sh .\gitClone.sh .\nembidRepo.list  C:\repo\nem\
+rootDir=$PWD
 while read line; do
-  echo "------------------------------------------------------------------------"
-  echo   Pulling Repo:$url 
-  echo "------------------------------------------------------------------------"
   proto=$(echo $line | cut -f 1 -d :)
   url=$(echo $line | cut -f 2 -d :)
   url="${proto}:${url}"
   tag=$(echo $line | cut -f 3 -d :)
   repo=$(echo $url | cut -f 4 -d /)
-  dir=$(echo $url | cut -f 5 -d /)
-  cd ./$dir && git pull && git submodule update --recursive --init && cd ..
+  repoDir=$(echo $url | cut -f 5 -d /)
+  echo "------------------------------------------------------------------------"
+  echo   Pulling Repo:$url 
+  echo "------------------------------------------------------------------------"
+  cd $2/$repoDir && git pull && git submodule update --recursive --init && cd $rootDir
 done < $1
